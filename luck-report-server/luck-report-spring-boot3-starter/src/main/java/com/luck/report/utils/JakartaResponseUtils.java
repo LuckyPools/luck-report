cc4396842e66bfc6220aee70a01c8034a2c0864d
quzhengrong123 <quzhengrong123@gmail.com>
2026-07-22 19:19:00 +0800

    更新项目版本号到 1.0.1 ，支持多版本的 Spring Boot

---
+++ luck-report-server/luck-report-spring-boot3-starter/src/main/java/com/luck/report/utils/JakartaResponseUtils.java
@@ -0,0 +1,34 @@
+package com.luck.report.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+
+public class JakartaResponseUtils {
+
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public static void writeObjectToJson(HttpServletResponse resp, Object obj) throws IOException {
+        resp.setContentType("text/json");
+        resp.setCharacterEncoding("UTF-8");
+        OutputStream out = resp.getOutputStream();
+        try {
+            objectMapper.writeValue(out, obj);
+        } finally {
+            out.flush();
+            out.close();
+        }
+    }
+}
